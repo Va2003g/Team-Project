72373a1d42aa4b14649c6d17f070aa5f2bfde466
GuptaVansh2003 <101311432+Va2003g@users.noreply.github.com>
2022-05-12 13:52:13 +0530

    Adding useful comments in the file.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Fetching all the important items
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
@@ -6,7 +7,7 @@ let formClose = document.querySelector('#form-close');
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let videoBtn = document.querySelectorAll('.vid-btn');
-
+// Creating functions using javascript..
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
